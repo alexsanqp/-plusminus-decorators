59e0716e0b3aab58e13e3722bfe099b93053611f
alexsanqp <alexsanqp@gmail.com>
2020-11-06 02:25:14 +0200

    (refactoring) Rename package name of library to `@plus-minus/decorators`

--- projects/plus-minus/decorators/karma.conf.js
+++ projects/plus-minus/decorators/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/plusminus/decorators'),
+      dir: require('path').join(__dirname, '../../../coverage/plus-minus/decorators'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
